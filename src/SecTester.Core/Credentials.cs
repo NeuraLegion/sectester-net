d67d28a9da870bac24e4a19e6e6001588117cf77
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2022-11-07 23:22:45 +0400

    feat(core): provide the ability to set a credentials (#7)
    closes #4

---
+++ src/SecTester.Core/Credentials.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace SecTester.Core
+{
+  public class Credentials
+  {
+    private readonly Regex _tokenValidationRegexp = new(@"^[A-Za-z0-9+/=]{7}\.nex[apr]\.[A-Za-z0-9+/=]{32}$");
+    public string Token { get; }
+
+    public Credentials(string token)
+    {
+      Token = token ?? throw new ArgumentNullException(nameof(token), "Provide an API key.");
+
+      if (!_tokenValidationRegexp.IsMatch(token))
+      {
+        throw new InvalidOperationException("Unable to recognize the API key.");
+      }
+    }
+  }
+}

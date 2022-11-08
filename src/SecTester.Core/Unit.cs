2e9e3c8d33a32ba44a8de48066aba3cbd994377f
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2022-11-08 13:17:52 +0400

    refactor(core): add a custom void type
    Introduce a custom void type (i.e. `Unit`) since `System.Void` is not a valid return type
    
    closes #23

---
+++ src/SecTester.Core/Unit.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+
+namespace SecTester.Core;
+
+// Based on https://github.com/jbogard/MediatR/blob/master/src/MediatR.Contracts/Unit.cs
+public readonly struct Unit : IEquatable<Unit>, IComparable<Unit>, IComparable
+{
+  private static readonly Unit _value = new();
+
+  public static ref readonly Unit Value => ref _value;
+
+  public static Task<Unit> Task { get; } = System.Threading.Tasks.Task.FromResult(_value);
+
+  public int CompareTo(Unit other) => 0;
+
+  int IComparable.CompareTo(object? obj) => 0;
+
+  public override int GetHashCode() => 0;
+
+  public bool Equals(Unit other) => true;
+
+  public override bool Equals(object? obj) => obj is Unit;
+
+  public static bool operator ==(Unit first, Unit second) => true;
+
+  public static bool operator !=(Unit first, Unit second) => false;
+
+  public override string ToString() => "()";
+}

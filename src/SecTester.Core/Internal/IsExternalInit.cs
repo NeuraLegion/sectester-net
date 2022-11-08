50cb3e40c41af28e5bffdb2d05f3fef03da378b3
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2022-11-08 13:20:03 +0400

    refactor(core): exclude `IsExternalInit` from code coverage
    closes #23

--- src/SecTester.Core/Internal/IsExternalInit.cs
+++ src/SecTester.Core/Internal/IsExternalInit.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 
 namespace System.Runtime.CompilerServices
 {
+  [ExcludeFromCodeCoverage]
   [EditorBrowsable(EditorBrowsableState.Never)]
   internal static class IsExternalInit { }
 }

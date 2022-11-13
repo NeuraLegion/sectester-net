50ba5d38ffe9a5ddfb8e9388ed4a4f54c7d852fc
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2022-11-14 01:53:23 +0400

    refactor(bus): encapsulate fields using protected init only setter
    closes #26

--- src/SecTester.Bus/Commands/HttpRequest.cs
+++ src/SecTester.Bus/Commands/HttpRequest.cs
@@ -6,10 +6,10 @@ namespace SecTester.Bus.Commands;
 
 public record HttpRequest<TResult> : Command<TResult>
 {
-  public readonly string? Body;
-  public readonly HttpMethod? Method;
-  public readonly Dictionary<string, string>? Params;
-  public readonly string Url;
+  public string? Body { get; protected init; }
+  public HttpMethod? Method { get; protected init; }
+  public Dictionary<string, string>? Params { get; protected init; }
+  public string Url { get; protected init; }
 
   public HttpRequest(string? url, HttpMethod? method = null, Dictionary<string, string>? @params = null,
     string? body = null, bool? expectReply = null, int? ttl = null) : base(expectReply, ttl)

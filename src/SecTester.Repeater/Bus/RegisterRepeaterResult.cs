e7c14bb1ebea1a85e36bf7ea1811d81f640b6d54
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2022-11-29 19:28:03 +0400

    refactor(repeater): mark `Version` as optional field
    closes #74

--- src/SecTester.Repeater/Bus/RegisterRepeaterResult.cs
+++ src/SecTester.Repeater/Bus/RegisterRepeaterResult.cs
@@ -1,3 +1,3 @@
 namespace SecTester.Repeater.Bus;
 
-public record RegisterRepeaterResult(string? Version, RepeaterRegisteringError Error = RepeaterRegisteringError.None);
+public record RegisterRepeaterResult(string? Version = default, RepeaterRegisteringError Error = RepeaterRegisteringError.None);

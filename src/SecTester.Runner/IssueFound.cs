ed74b547534f86e7ff5e8bd98842483b1051193a
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2022-12-09 19:15:51 +0400

    feat(runner): implement a test builder
    closes #98

---
+++ src/SecTester.Runner/IssueFound.cs
@@ -0,0 +1,16 @@
+using SecTester.Core.Exceptions;
+using SecTester.Reporter;
+using SecTester.Scan.Models;
+
+namespace SecTester.Runner;
+
+public class IssueFound : SecTesterException
+{
+  public Issue Issue { get; }
+
+  public IssueFound(Issue issue, Formatter formatter) :
+    base($"Target is vulnerable\n\n{formatter.Format(issue)}")
+  {
+    this.Issue = issue;
+  }
+}

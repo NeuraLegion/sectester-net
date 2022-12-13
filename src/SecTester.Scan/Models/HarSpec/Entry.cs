e8aa7851dee8efe7ff18c94a66ad5b7fbe4aa5fb
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2022-12-13 12:50:32 +0400

    fix(scan): rename fields according to HAR spec
    closes #113

--- src/SecTester.Scan/Models/HarSpec/Entry.cs
+++ src/SecTester.Scan/Models/HarSpec/Entry.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace SecTester.Scan.Models.HarSpec;
 
-public record Entry(DateTime StartedDateTime, RequestMessage RequestMessage, ResponseMessage ResponseMessage, Timings Timings, Cache Cache)
+public record Entry(DateTime StartedDateTime, RequestMessage Request, ResponseMessage Response, Timings Timings, Cache Cache)
 {
   public int Time { get; init; }
 }
--- src/SecTester.Scan/Models/HarSpec/ResponseMessage.cs
+++ src/SecTester.Scan/Models/HarSpec/ResponseMessage.cs
@@ -1,3 +1,3 @@
 namespace SecTester.Scan.Models.HarSpec;
 
-public record ResponseMessage(int Status, string StatusText, string RedirectUrl, Content Content) : EntryMessage;
+public record ResponseMessage(int Status, string StatusText, string RedirectURL, Content Content) : EntryMessage;

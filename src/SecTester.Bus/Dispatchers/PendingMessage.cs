1eab724ede8223714f15d17461a839303f16d9f7
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2022-12-13 23:45:45 +0400

    fix(bus): use the same channel for consuming and publishing in RPC
    closes #125

--- src/SecTester.Bus/Dispatchers/PendingMessage.cs
+++ src/SecTester.Bus/Dispatchers/PendingMessage.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace SecTester.Bus.Dispatchers;
 
@@ -11,4 +12,8 @@ internal sealed record MessageParams<T>
   public string? CorrelationId { get; init; }
   public string? ReplyTo { get; init; }
   public DateTime? CreatedAt { get; init; }
+
+  public string ToJson() => Payload is not null ? MessageSerializer.Serialize(Payload) : "";
+  public byte[] ToBytes() => Encoding.UTF8.GetBytes(ToJson());
 }
+
